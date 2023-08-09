480f0de85bf6b53bcf7ddcfd4692d029d6f39036
Jan <pcerservices@gmail.com>
2023-08-09 16:02:42 +0200

    Removed unused import in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Note from './components/Note';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
